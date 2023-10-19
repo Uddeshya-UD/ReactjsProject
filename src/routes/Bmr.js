2ad3b4d3a91103a5b9d4f3368994083eb380d0fe
Uddeshya-UD <uddeshya31@gmail.com>
2023-10-19 11:25:26 +0530

    improved form-heading

--- src/routes/Bmr.js
+++ src/routes/Bmr.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import BmrCalculator from '../components/BmrCalculator'
 import Navbar from '../components/Navbar' 
-import Footer from '../components/Footer'
 
 const Bmr = () => {
   return (
     <div>
       <Navbar/>
       <BmrCalculator/>
-      {/* <Footer/> */}
+
       </div>
   )
 }
