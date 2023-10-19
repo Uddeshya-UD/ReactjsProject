d02aefa69137d44bd1c9133bab2430e75626177d
Uddeshya-UD <61532048+Uddeshya-UD@users.noreply.github.com>
2023-10-19 11:31:06 +0530

    Update Login.js

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navbar from '../components/Navbar' ;
-import Footer from '../components/Footer';
 import LoginForm from '../components/LoginF';
 const Register = () => {
   return (
     <div>
       <Navbar/>
       <LoginForm/>
-      {/* <Footer/> */}
           </div>
   )
 }
